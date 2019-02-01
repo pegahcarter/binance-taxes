550d1d62c0ac4c507edf6afea05b068423efdf85
Carter Carlson <cartercarlson94@gmail.com>
2019-01-31 21:28:36 -0600

    Add basic code to pull tickers from the exchange

--- py/functions.py
+++ py/functions.py
@@ -3,6 +3,19 @@ import numpy as np
 from datetime import datetime
 import ccxt
 
+# -------------------------------------------------------------------------
+# Testing
+market = exchange.load_markets()
+tickers = list(market.keys())
+
+coins = set()
+[[coins.add(coin) for coin in ticker.split('/') if coin != 'BTC'] for ticker in tickers]
+coins = list(coins)
+
+# Since we can't pull BTC/BTC, use BTC/USDT ticker.  Otherwise, use coin/BTC as ticker
+tickers = [coin + '/BTC' for coin in coins]
+
+# -------------------------------------------------------------------------
 exchange = ccxt.binance()
 def btc_price(coin, date):
     if coin == 'BTC':
@@ -11,18 +24,12 @@ def btc_price(coin, date):
         return exchange.fetch_ohlcv(symbol=coin + '/BTC', since=date, limit=1)[0][2]
 
 
-
-
-
-
-
-
 def addCoin(coin, coinUnits, date=None, currentPrice=None):
     ''' Add initial purchase of coin to transactions table '''
 
     if date is None:
         date = datetime.now()
-        currentPrice = exchange.price(coin)
+        currentPrice = exchange.fetch_price(coin)
 
 
     df = pd.read_csv(TRANSACTIONS_FILE)
@@ -60,7 +67,7 @@ def update(coins, sides, coinUnits, d_amt, date=None, currentPrice=None):
 
         if date is None:
             date = datetime.now()
-            currentPrice = exchange.price(coin)
+            currentPrice = exchange.fetch_price(coin)
 
         if tradeSide == 'buy':
             fees = d_amt * 0.00075
